cb3bf8405e459c91d958354050a851ebf5204ee6
Ajay kumar <ajayjeyapal@gmail.com>
2023-09-23 19:39:54 +0530

    main

---
+++ app.py
@@ -0,0 +1,173 @@
+from flask import Flask,request,url_for,render_template,session,redirect
+import mysql.connector
+import json
+app = Flask(__name__)
+
+days=3
+app.secret_key="ajaykumar"
+app.permanent_session_lifetime=days*24*60*60
+
+                        # connecting to mysql database
+mydb = mysql.connector.connect(
+        host="localhost",
+  	    username="root",
+  	    password="Ajay@2002",
+        database="mylearn"
+)
+
+mycursor = mydb.cursor(dictionary=True)
+
+
+@app.route('/test',methods=['GET', 'POST'])
+def test():
+    if request.method == "GET":
+        return render_template("test.html")
+
+
+@app.route('/dashboard',methods=['GET', 'POST'])
+def dashboard():
+    if request.method == "GET":
+        if session['uemail'] is not None :
+            msg="tomato"
+            return render_template("dashboard.html", msg=msg)
+        else :
+            
+            return redirect(url_for("sessioncheck"))
+        
+        
+    
+@app.route('/',methods=['GET', 'POST'] )
+def sessioncheck():
+    #session['uemail'] = None
+    #session.pop('uemail',None)
+    if request.method=="GET":
+        if session.get('uemail') is not None:
+            return redirect("/dashboard")
+        else :
+            return render_template("index.html")
+    elif request.method=="POST":
+        return "eppadi da panna itha ethuku da unnaku intha polapuu "
+    
+    
+@app.route('/My_Auction',methods=['GET', 'POST'] )
+def myauction():
+    if request.method=="GET":
+        if session.get('uemail')  is not None:
+            return render_template("myauction.html")
+        else:
+            return redirect(url_for("sessioncheck"))
+        
+    else:
+        return "uui"
+    
+@app.route('/logout',methods=['GET', 'POST'] )
+def logout():
+    if request.method=="GET":
+        if session.get('uemail')  is not None:
+            session['uemail']=None
+            session["username"] = None
+            session["unumber"] = None
+            session['password'] = None
+            return redirect(url_for("sessioncheck"))
+        else:
+            return redirect(url_for("sessioncheck"))
+    else:
+        return redirect(url_for("sessioncheck"))
+        
+
+            
+    
+    
+    
+                #getting the input and stored in session and mysql database  
+@app.route('/register',methods=['GET', 'POST'])
+def get():
+    if request.method == "POST":
+
+                # getting the user inputs
+        username = request.form.get('uname')
+        useremail = request.form.get('uemail')
+        usernumber = request.form.get('unumber')
+        userpassword = request.form.get('upass')
+        if useremail and username and usernumber and userpassword:
+            
+                # storing in session
+                session["username"] = username
+                session["uemail"] = useremail
+                session["unumber"] = usernumber
+                session['password'] = userpassword
+                
+                try:
+                    mycursor=mydb.cursor(dictionary=True)
+                    mycursor.execute("select name from userinfo where name=%s", (username,))
+                    myresult2=mycursor.fetchall()    
+                    mycursor.execute("select email from userinfo where email=%s", (useremail,))
+                    myresult1=mycursor.fetchall()
+                    mycursor.execute("select number from userinfo where number=%s", (usernumber,))
+                    myresult3=mycursor.fetchall()
+                    
+                except Exception as e:
+                    return f"Error: {e} Error Occurred please try Again later "
+                    
+               
+                if not myresult1  and   not myresult2 and not myresult3 :
+                    mycursor.execute(" insert into userinfo (name,email,number,password ) values(%s,%s,%s,%s)" ,(username,useremail,usernumber,userpassword))
+                    mydb.commit()
+                    return "ok"
+                elif myresult1  and   not myresult2 and not myresult3 : 
+                    return "This email id already have an account"
+                elif not myresult1  and   myresult2 and not myresult3 : 
+                    return "This username already taken"
+                elif not myresult1  and   not myresult2 and  myresult3 : 
+                    return "This phone number already have an account"
+                elif myresult1 and myresult2 and not myresult2:
+                    return " This email id and username already Exists"
+                elif not myresult1 and myresult2 and myresult3:
+                    return "This username and phone number Already Exits"
+                elif myresult1 and not myresult2 and myresult3:
+                    return "This Email id and phone number Already have an Account"
+                elif myresult1 and myresult2 and myresult3:
+                    return "Already an account exists with this email id ,username and phone number "
+                    
+                    
+                else:
+                    return " from server Something Went Wong please try Again Later"
+        else:
+            return "Enter all the details and then click"
+    else:
+        return  redirect(url_for("sessioncheck"))
+    
+                # login functions
+@app.route( '/login', methods=["GET",  "POST"] )
+def login():
+    if request.method == "POST":
+        useremail = request.form.get('lemail')
+        userpassword = request.form.get('lpass')
+        if useremail and userpassword:
+            mycursor.execute("select email from userinfo where email=%s" ,(useremail,))
+            result1 = mycursor.fetchone()
+            
+            if result1:
+                mycursor.execute("select password from userinfo where email=%s" ,(useremail,))
+                result2 = mycursor.fetchone()
+                dpassword=result2["password"]
+                if dpassword==userpassword:
+                    session["uemail"] = useremail
+                    session['password'] = userpassword
+                    return "ok"
+                else:
+                    return "You Entered a incorrect Password"
+            else:
+                return "no user in this email" 
+        else:
+            return "Enter your details and then click" 
+    else:
+        return redirect(url_for("sessioncheck"))
+        
+    
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+    
---
+++ static/javascript/myscript.js
@@ -0,0 +1,281 @@
+function myfun(){
+
+    back = document.getElementById("back")
+    back.style.display = "flex"
+    
+    register = document.getElementById("reg");
+    register.style.display = "flex";
+    
+}
+function login2() {
+    
+
+
+    back = document.getElementById("back")
+    back.style.display = "flex"
+    
+    register = document.getElementById("reg2");
+    register.style.display = "flex";
+    
+}
+
+function myfun2(){
+
+    back = document.getElementById("back")
+    back.style.display = "none"
+    
+    register = document.getElementById("reg");
+    register.style.display = "none";
+    
+}
+
+function myfun3(){
+
+    back = document.getElementById("back")
+    back.style.display = "none"
+    
+    register = document.getElementById("reg2");
+    register.style.display = "none";
+    
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Your code here, including the event listener registration
+document.getElementById("signform").addEventListener("submit", function(event) {
+    // Prevent the form from submitting
+    event.preventDefault();
+    
+
+
+    //collecting input values
+     
+     username = document.getElementById("name").value;
+     useremail = document.getElementById("email").value;
+     usernumber = document.getElementById("number").value;
+     userpassword = document.getElementById("password").value;
+     form = document.getElementById("signform");
+     msg = document.getElementById("signmsg")
+
+
+
+    // validating if input field is empty or not
+
+    unumber = usernumber.trim()
+    num = unumber.length
+
+    
+    if (username.trim()==""){
+        alert("Enter Your Name and then click Submit")
+        return false;
+
+    }
+
+    if (useremail.trim()==""){
+        alert("Enter Your Email id and then click Submit")
+        return false;
+
+    }
+    if (usernumber.trim()==""){
+        alert("Enter Your Phone Number and then click Submit")
+        return false;
+
+    }
+    
+    if (userpassword.trim()==""){
+        alert("Enter Your Password and then click Submit")
+        return false;
+
+    }
+
+    if (num !== 10){
+        alert("Enter a valid  Phone Number and then click Submit")
+        return false;
+        
+
+    }
+    showloading();
+
+    fetch("/register",{
+        method:"POST",
+        body: new FormData(form)
+        
+    })
+    .then(res=>{
+        console.log(res)
+        if (!res.ok){
+            throw new error("Something Went wrong please try again later ")
+        }
+        else{
+            return res.text();
+        }
+    })
+    .then(data=>{
+        console.log(data)
+        setTimeout(() => {
+            hideloading2();
+            if (data=="ok"){
+                window.location.href="/dashboard"
+            }
+            else{
+                msg.style.color="red"
+                msg.innerHTML=data
+            }
+        }, 5000);
+    })
+
+    .catch(error=>{
+        setTimeout(() => {
+            hideloading2()
+            msg.style.color="red"
+            msg.innerHTML=error
+            console.log(error)
+            }, 5000);
+    })
+
+})})
+
+function showloading(){
+    load = document.getElementById("loading");
+    load.style.display="block"
+    register = document.getElementById("reg2");
+    register.style.display = "none";
+    register = document.getElementById("reg");
+    register.style.display = "none";
+}
+
+function hideloading1(){
+    load = document.getElementById("loading");
+    load.style.display="none"
+    register = document.getElementById("reg2");
+    register.style.display = "block";
+}
+
+function hideloading2(){
+    load = document.getElementById("loading");
+    load.style.display="none"
+    register = document.getElementById("reg");
+    register.style.display = "block";
+}
+
+
+
+
+
+                            // login form validate 
+document.addEventListener("DOMContentLoaded", function() {
+document.getElementById("login").addEventListener("submit", function(event){ 
+    event.preventDefault();
+
+            // getting the required elements
+    form=document.getElementById("login")
+    msg=document.getElementById("msg")
+    useremail = document.getElementById("lemail").value;
+    userpassword = document.getElementById("lpassword").value;
+
+            // check whether the input box is empty or not
+    if (useremail.trim()==""){
+        alert("Enter Your Email id and then click Submit")
+        return false;
+
+    }
+
+    if (userpassword.trim()==""){
+        alert("Enter Your Password and then click Submit")
+        return false;
+
+    }
+        // preventing the default form submission and shows loading screen
+    
+    showloading();
+
+        // sending login form data to server and handiling the response 
+    fetch("/login", {
+        method: "POST",
+        body: new FormData(form)
+    })
+
+    .then(res=>{
+        
+        if (!res.ok){
+            
+            throw new Error("Error in fetching your data please try again later");
+        }
+        else{
+            return res.text()
+        }
+    })
+
+    .then(data=>{
+        setTimeout(() => {
+            hideloading1();
+
+
+
+            if (data=="ok"){
+
+                window.location.href="/dashboard";
+    
+            }
+            else{
+                msg.style.color="red"
+                msg.innerHTML=data;
+                
+            }
+            
+        }, 5000);
+    })
+
+    .catch(error=>{
+        setTimeout(() => {
+            hideloading1();
+            msg.style.color="red"
+            msg.innerHTML=error;
+            console.log(error)
+            
+        }, 5000);
+        
+    });
+
+})});
+
+
+
+function myfetch() {
+    fetch("/register")
+        .then(res =>{
+            console.log(res)
+            return res.text();
+        })
+
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Fetch error:', error)
+        });
+
+    window.location.href= "/"
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    classs=document.querySelectorAll(".bidbutton")
+    classs.forEach(element => {
+        element.addEventListener("click",function(event){
+            var element =event.target
+            var dataid = element.getAttribute("data-id")
+            blur = document.getElementById("blur")
+            blur.style.display="flex";
+            bidding = document.getElementById("bidding")
+            bidding.style.display="flex";
+            alert(dataid)
+        
+    });
+            
+
+
+
+    })
+
+})
